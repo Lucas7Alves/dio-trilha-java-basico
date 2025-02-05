539fa4f25fd18a58d01350ce201facd9f026835d
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2025-02-05 17:38:03 -0300

    DesafioControleFluxo

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -4,24 +4,9 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Por favor, digite o número da conta !");
-        int numeroAgnecia = sc.nextInt();
+        String[] nomes = { "Camila", "Venilton", "Leonardo", "Renan", "Rafael" };
+        System.out.print(nomes.length);
 
-        sc.nextLine();
-        System.out.println("Por favor, digite o número da agência !");
-        String agencia = sc.nextLine();
-
-        System.out.println("Por favor, digite o seu nome !");
-        String nome = sc.nextLine();
-
-        System.out.println("Por favor, digite o saldo da conta !");
-        double saldo = sc.nextDouble();
-
-        String msg = "Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
-                + agencia + ", conta " + numeroAgnecia
-                + " e seu saldo " + saldo + " já está disponível para saque";
-
-        System.out.println(msg);
-
+        sc.close();
     }
 }
