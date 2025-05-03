33962542efecb5df312be8c006707bc8fb49b1c4
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2025-05-02 21:23:09 -0300

    cloud project, board project

---
+++ inCloud/bradesco/bradesco/src/test/java/me/dio/bradesco/BradescoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.bradesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradescoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
